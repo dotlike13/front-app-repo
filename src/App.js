55220fb27fe3bc718a1bc759fd5c9f303ba8f1aa
jdlee-test <test@aaa.com>
2023-05-22 14:56:03 +0000

    Add new blog version 2

--- src/App.js
+++ src/App.js
@@ -64,7 +64,7 @@ function App(props) {
             align="center"
             className={classes.title}
           >
-            EKS DEMO Blog versions 1
+            EKS DEMO Blog versions 2
           </Typography>
           {new Date().toLocaleTimeString()}
         </Toolbar>
